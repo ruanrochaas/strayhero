4dfc39a93cf9048b7ab5260a91af9c130ad7e492
ruanrochaas <ruansouza@alu.ufc.br>
2019-06-04 15:10:08 -0300

    grosso do backend feito

--- strayhero-server2/app/models/animal.js
+++ strayhero-server2/app/models/animal.js
@@ -2,17 +2,21 @@ let mongoose = require('mongoose');
 
 module.exports = function () {
     let schema = mongoose.Schema({
-        nome: { type: String, required: true },
-        tipo: { type: String, required: true },
+        nome: { type: String, required: true, unique : true },
+        tipo: { type: String, required: true }/*, 
         dataDoResgate: { type: Date, required: true },
-        morto: { type: Boolean, required: true },
-        itensUsados: { type: Array, required: true },
+        morto: { type: Boolean, required: true }, */
+        /* itensUsados: { type: Array, required: true }, */
+        /* dataVacina: { type: Number, required: true },
         estaVacinado: { type: Boolean, required: true },
-        horaAlimentacao: { type: Number, required: true },
+        dataComida: { type: Number, required: true },
         statusComida: { type: Number, required: true },
+        dataAgua: { type: Number, required: true },
         statusAgua: { type: Number, required: true },
+        dataSaude: { type: Number, required: true },
         statusSaude: { type: Number, required: true },
-        statusAtencao: { type: Number, required: true }
-    });
+        dataAtencao: { type: Number, required: true },
+        statusAtencao: { type: Number, required: true } */
+    }, {collection:'animais'});
     return mongoose.model('Animal', schema);
 }();
